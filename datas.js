08f920a6f0ca1f46f6b9cb89e7e5e6007ca103af
bentley-senf <107149974+bentley-senf@users.noreply.github.com>
2022-06-30 00:55:11 +0200

    secret message

---
+++ datas.js
@@ -0,0 +1,322 @@
+// a
+var decode_a_1 = "1WpyjYn{G@Go#;-";
+// b
+var decode_b_1 = "_!C9gN=1g=RY3T(";
+// c
+var decode_c_1 = "pc(ckS{,Wf$Ng42";
+// d
+var decode_d_1 = "I9s!]$zV_gCq06w";
+// e
+var decode_e_1 = "9MsA#IJZ]_N+-(B";
+// f
+var decode_f_1 = "dYWnNdle'EloKLl";
+// g
+var decode_g_1 = "2ZhoC0}R@6{Z,$v";
+// h
+var decode_h_1 = "bruyiXr6O.)#Vmt";
+// i
+var decode_i_1 = "wJZ$[46I6cJ%O{S";
+// j
+var decode_j_1 = "jRl~vP#,=-%w3jq";
+// k
+var decode_k_1 = "l3'-7gBJh,VbgiF";
+// l
+var decode_l_1 = "jPvoUcV&';'j_8W";
+// m
+var decode_m_1 = "ngJUK0A1D=Y!+Ef";
+// n
+var decode_n_1 = "F;,o~WM+M2{b&P~";
+// o
+var decode_o_1 = "iIW,69Eqf}3qg7v";
+// p
+var decode_p_1 = "M6LM(G1y}=die25";
+// q
+var decode_q_1 = "0ED&$&v-E4LSq2d";
+// r
+var decode_r_1 = "F69H!Kg2=e#R'11";
+// s
+var decode_s_1 = "TV8V2pY(oub$N.g";
+// t
+var decode_t_1 = "_m=))TxZWuIMfuJ";
+// u
+var decode_u_1 = "T!tF)o3(GU0V0Pq";
+// v
+var decode_v_1 = "7,Q0X~e,(0hAvZ[";
+// w
+var decode_w_1 = "G4bc(P{6oXNHV2c";
+// x
+var decode_x_1 = "v9XaZ30SC6M3&yz";
+// y
+var decode_y_1 = "]gr)l4He_K85#XJ";
+// z
+var decode_z_1 = "(eQe+{CU~u{o0O!";
+// ß
+var decode_ß_1 = "39Q6WCKD#BY-[p2";
+// A
+var decode_A_1 = "g@w$yLy7MaV9a6q";
+// B
+var decode_B_1 = "p1f#nJlOv?ns4$7";
+// C
+var decode_C_1 = "zd2QaQ9@3kCdtjt";
+// D
+var decode_D_1 = "J@?oW3IPB5XhbvF";
+// E
+var decode_E_1 = "S&=bsF,$}BJM9L(";
+// F
+var decode_F_1 = "UA#h4mBhfy=ev=v";
+// G
+var decode_G_1 = "U}kc:~,wa8sLg>U";
+// H
+var decode_H_1 = "RX-Farh4B#<6?2}";
+// I
+var decode_I_1 = "E{Y)=MFdBVsyHux";
+// J
+var decode_J_1 = "U(TqE6&bS7>h%k&";
+// K
+var decode_K_1 = ")#Er?eWr_kg@s(P";
+// L
+var decode_L_1 = "e)LX8k$}GgSATM#";
+// M
+var decode_M_1 = "WVrJ6Va&V>+RtAD";
+// N
+var decode_N_1 = "$uPd-vN$k9sL2_X";
+// O
+var decode_O_1 = "%RfqYR,8<.Q&{7_";
+// P
+var decode_P_1 = "TvB3!>mwF>.dvH+";
+// Q
+var decode_Q_1 = "5,CA.WmGS@B:)nk";
+// R
+var decode_R_1 = "7§VJ6P-fF=mGQ>9";
+// S
+var decode_S_1 = "K{!L{8N~b)!XxR%";
+// T
+var decode_T_1 = "hV}z(jcgmWLZ=6p";
+// U
+var decode_U_1 = "N$?8_)E#@wWNgec";
+// V
+var decode_V_1 = "w%#uB+xx>8}rc%<";
+// W
+var decode_W_1 = "<<qB@?g(~3&npR,";
+// X
+var decode_X_1 = "Qe)V&L=@2aDNfa(";
+// Y
+var decode_Y_1 = "u+5J!=H~c$2fUP4";
+// Z
+var decode_Z_1 = "pt<h62sMD4vyJZQ";
+//ä
+var decode_ä_1 = "8PCkd&D^$fn2*ML";
+//ö
+var decode_ö_1 = "8Ez@&F$@6GM8Q9o";
+//ü
+var decode_ü_1 = "Ghr#%Jw*^IOX6j7";
+//Ä
+var decode_Ä_1 = "LTyta^HSC8un#y0";
+//Ö
+var decode_Ö_1 = "Kf^XQsDd8xBI*oU";
+//Ü
+var decode_Ü_1 = "!BWds#eiykRW#xW";
+// 0
+var decode_0_1 = "P+,@a-k§Q4s_(_}";
+// 1
+var decode_1_1 = "?U,nJ&QR_r<§rFP";
+// 2
+var decode_2_1 = "7~zRxr$KS.<v)we";
+// 3
+var decode_3_1 = "dmj#z$?#scwTL5?";
+// 4
+var decode_4_1 = "$z)<BLzcx}fB@2!";
+// 5
+var decode_5_1 = "3&%nQcD(Bb-{)v!";
+// 6
+var decode_6_1 = "n.m#E:)&3dc)_L7";
+// 7
+var decode_7_1 = "pW}kU$F<9Z+MUQ§";
+// 8
+var decode_8_1 = "<s&9,Z,L:Z2Qy36";
+// 9
+var decode_9_1 = "5U!cZre@<M+M<XB";
+// 
+var decode_space_1 = "z&7hDze!TWeu#dS";
+// ^
+var decode_caret_1 = "rwWy<?+5)$d<%>u";
+//°
+var decode_bullet_1 = "yX}uRPf+X>#u4)9";
+//!
+var decode_exclamation_mark_1 = ".cVBsv8rZyW§RLq";
+//"
+var decode_quotation_mark_1 = "LC}eXh}syjhHEdP";
+//§
+var decode_section_sign_1 = "_p%VySFZcQ@CxWz";
+//$
+var decode_dollar_sign_1 = "Hdh<tPm~c&f-5VL";
+//%
+var decode_percent_1 = "s&h:N_(3J}kNMUW";
+//&
+var decode_and_1 = "MX)s#BaX}4~#sFk";
+///
+var decode_slash_1 = "(Yx$>x@eL&YR+-E";
+//\
+var decode_backslash_1 = "(Yx$>x@eL&YR+-E";
+//(
+var decode_bracket_left_1 = "(Yx$>x@eL&YR+-E";
+//)
+var decode_bracket_right_1 = "X3F.XSZ.X$,v4&k";
+//{
+var decode_curly_bracket_left_1 = "qt,RM2L=M%fMk?y";
+//}
+var decode_curly_bracket_right_1 = "euEG_ApvP,#jL7M";
+//[
+var decode_square_bracket_left_1 = "}::37S5h:qct,}x";
+//]
+var decode_square_bracket_right_1 = "$X+§4n8(m@kpjhj";
+//<
+var decode_angle_bracket_left_1 = "C+dTcmFT_hfq@56";
+//>
+var decode_angle_bracket_right_1 = "-#,rsKbyYkN:b{F";
+//=
+var decode_equal_1 = ")wE+72s?dBZzx@t";
+//?
+var decode_question_mark_1 = "b-<}2vS(uA42wRH";
+//´
+var decode_acute_accent_1 = "2aDrEg.j6x(Z(RH";
+//`
+var decode_grave_accent_1 = "rzjgeh_:P+e@maa";
+//+
+var decode_plus_1 = "$}5LCzqH8)GR$H4";
+//*
+var decode_asterisk_1 = "zH_+Za>KWmZeA5t";
+//~
+var decode_tilde_1 = "w2WZ.wU9?z.Sex#";
+//#
+var decode_hash_1 = "QvJc_}uv4RrXk5v";
+//-
+var decode_minus_1 = "(Dna?<y3.W:Q>5<";
+//_
+var decode_understrike_1 = "=.L#fxUFvP+<AYH";
+//.
+var decode_dot_1 = "s,%9Mq:,.Vhn:@G";
+//:
+var decode_colon_1 = "VrayyS§cG>q8}=E";
+//,
+var decode_comma_1 = "9CVn.3<>PbrmAm+";
+//;
+var decode_semicolon_1 = "a8W2Eszm39>B~x6";
+//@
+var decode_at_1 = "bF9t)XNnfE_9tCS";
+//|
+var decode_pipe_1 = ".4qd}EEEJm7>+3D";
+//'
+var decode_apostroph_1 = "py45MbPWQ=B6w{{";
+//€
+var decode_euro_1 = "p!+3§.#BtTKv&g(";
+
+
+window.codes = function(){
+    coder1(variablee, "a", decode_a_1);
+    coder1(variablee, "b", decode_b_1);
+    coder1(variablee, "c", decode_c_1);
+    coder1(variablee, "d", decode_d_1);
+    coder1(variablee, "e", decode_e_1);
+    coder1(variablee, "f", decode_f_1);
+    coder1(variablee, "g", decode_g_1);
+    coder1(variablee, "h", decode_h_1);
+    coder1(variablee, "i", decode_i_1);
+    coder1(variablee, "j", decode_j_1);
+    coder1(variablee, "k", decode_k_1);
+    coder1(variablee, "l", decode_l_1);
+    coder1(variablee, "m", decode_m_1);
+    coder1(variablee, "n", decode_n_1);
+    coder1(variablee, "o", decode_o_1);
+    coder1(variablee, "p", decode_p_1);
+    coder1(variablee, "q", decode_q_1);
+    coder1(variablee, "r", decode_r_1);
+    coder1(variablee, "s", decode_s_1);
+    coder1(variablee, "t", decode_t_1);
+    coder1(variablee, "u", decode_u_1);
+    coder1(variablee, "v", decode_v_1);
+    coder1(variablee, "w", decode_w_1);
+    coder1(variablee, "x", decode_x_1);
+    coder1(variablee, "y", decode_y_1);
+    coder1(variablee, "z", decode_z_1);
+    coder1(variablee, "ß", decode_ß_1);
+    coder1(variablee, "A", decode_A_1);
+    coder1(variablee, "B", decode_B_1);
+    coder1(variablee, "C", decode_C_1);
+    coder1(variablee, "D", decode_D_1);
+    coder1(variablee, "E", decode_E_1);
+    coder1(variablee, "F", decode_F_1);
+    coder1(variablee, "G", decode_G_1);
+    coder1(variablee, "H", decode_H_1);
+    coder1(variablee, "I", decode_I_1);
+    coder1(variablee, "J", decode_J_1);
+    coder1(variablee, "K", decode_K_1);
+    coder1(variablee, "L", decode_L_1);
+    coder1(variablee, "M", decode_M_1);
+    coder1(variablee, "N", decode_N_1);
+    coder1(variablee, "O", decode_O_1);
+    coder1(variablee, "P", decode_P_1);
+    coder1(variablee, "Q", decode_Q_1);
+    coder1(variablee, "R", decode_R_1);
+    coder1(variablee, "S", decode_S_1);
+    coder1(variablee, "T", decode_T_1);
+    coder1(variablee, "U", decode_U_1);
+    coder1(variablee, "V", decode_V_1);
+    coder1(variablee, "W", decode_W_1);
+    coder1(variablee, "X", decode_X_1);
+    coder1(variablee, "Y", decode_Y_1);
+    coder1(variablee, "Z", decode_Z_1);
+    coder1(variablee, "ä", decode_ä_1);
+    coder1(variablee, "ö", decode_ö_1);
+    coder1(variablee, "ü", decode_ü_1);
+    coder1(variablee, "Ä", decode_Ä_1);
+    coder1(variablee, "Ö", decode_Ö_1);
+    coder1(variablee, "Ü", decode_Ü_1);
+    coder1(variablee, "1", decode_1_1);
+    coder1(variablee, "2", decode_2_1);
+    coder1(variablee, "3", decode_3_1);
+    coder1(variablee, "4", decode_4_1);
+    coder1(variablee, "5", decode_5_1);
+    coder1(variablee, "6", decode_6_1);
+    coder1(variablee, "7", decode_7_1);
+    coder1(variablee, "8", decode_8_1);
+    coder1(variablee, "9", decode_9_1);
+    coder1(variablee, "0", decode_0_1);
+    coder1(variablee, " ", decode_space_1);
+    coder1(variablee, "^", decode_caret_1);
+    coder1(variablee, "°", decode_bullet_1);
+    coder1(variablee, "!", decode_exclamation_mark_1);
+    coder1(variablee, '"', decode_quotation_mark_1);
+    coder1(variablee, "§", decode_section_sign_1);
+    coder1(variablee, "$", decode_dollar_sign_1);
+    coder1(variablee, "%", decode_percent_1);
+    coder1(variablee, "&", decode_and_1);
+    coder1(variablee, "/", decode_slash_1);
+    coder1(variablee, "\\", decode_backslash_1);
+    coder1(variablee, "(", decode_bracket_left_1);
+    coder1(variablee, ")", decode_bracket_right_1);
+    coder1(variablee, "{", decode_curly_bracket_left_1);
+    coder1(variablee, "}", decode_curly_bracket_right_1);
+    coder1(variablee, "[", decode_square_bracket_left_1);
+    coder1(variablee, "]", decode_square_bracket_right_1);
+    coder1(variablee, "<", decode_angle_bracket_left_1);
+    coder1(variablee, ">", decode_angle_bracket_right_1);
+    coder1(variablee, "=", decode_equal_1);
+    coder1(variablee, "?", decode_question_mark_1);
+    coder1(variablee, "´", decode_acute_accent_1);
+    coder1(variablee, "`", decode_grave_accent_1);
+    coder1(variablee, "+", decode_plus_1);
+    coder1(variablee, "*", decode_asterisk_1);
+    coder1(variablee, "~", decode_tilde_1);
+    coder1(variablee, "#", decode_hash_1);
+    coder1(variablee, "-", decode_minus_1);
+    coder1(variablee, "_", decode_understrike_1);
+    coder1(variablee, ".", decode_dot_1);
+    coder1(variablee, ":", decode_colon_1);
+    coder1(variablee, ",", decode_comma_1);
+    coder1(variablee, ";", decode_semicolon_1);
+    coder1(variablee, "@", decode_at_1);
+    coder1(variablee, "|", decode_pipe_1);
+    coder1(variablee, "'", decode_apostroph_1);
+    coder1(variablee, "€", decode_euro_1);
+}
---
+++ script.js
@@ -0,0 +1,316 @@
+
+function generate(){
+    let input = document.querySelector('#input').value;
+    const text = input.split("");
+    // var splitter = "";
+    var weg = new Boolean();
+    var gesamt;
+    var gesamt2;
+    var repeat = 0;
+    var exit = new Boolean();
+    text.forEach(variablee => {
+        exit = false;
+        weg = false;
+        function all(dirgesamt, dirgesamt2){
+            if(true == true){
+                function coder1(vari, letter, result){
+                    if(vari == letter){
+                        dirgesamt = dirgesamt2 + result;
+                    }
+                }
+                coder1(variablee, "a", decode_a_1);
+                coder1(variablee, "b", decode_b_1);
+                coder1(variablee, "c", decode_c_1);
+                coder1(variablee, "a", decode_a_1);
+                coder1(variablee, "b", decode_b_1);
+                coder1(variablee, "c", decode_c_1);
+                coder1(variablee, "d", decode_d_1);
+                coder1(variablee, "e", decode_e_1);
+                coder1(variablee, "f", decode_f_1);
+                coder1(variablee, "g", decode_g_1);
+                coder1(variablee, "h", decode_h_1);
+                coder1(variablee, "i", decode_i_1);
+                coder1(variablee, "j", decode_j_1);
+                coder1(variablee, "k", decode_k_1);
+                coder1(variablee, "l", decode_l_1);
+                coder1(variablee, "m", decode_m_1);
+                coder1(variablee, "n", decode_n_1);
+                coder1(variablee, "o", decode_o_1);
+                coder1(variablee, "p", decode_p_1);
+                coder1(variablee, "q", decode_q_1);
+                coder1(variablee, "r", decode_r_1);
+                coder1(variablee, "s", decode_s_1);
+                coder1(variablee, "t", decode_t_1);
+                coder1(variablee, "u", decode_u_1);
+                coder1(variablee, "v", decode_v_1);
+                coder1(variablee, "w", decode_w_1);
+                coder1(variablee, "x", decode_x_1);
+                coder1(variablee, "y", decode_y_1);
+                coder1(variablee, "z", decode_z_1);
+                coder1(variablee, "ß", decode_ß_1);
+                coder1(variablee, "A", decode_A_1);
+                coder1(variablee, "B", decode_B_1);
+                coder1(variablee, "C", decode_C_1);
+                coder1(variablee, "D", decode_D_1);
+                coder1(variablee, "E", decode_E_1);
+                coder1(variablee, "F", decode_F_1);
+                coder1(variablee, "G", decode_G_1);
+                coder1(variablee, "H", decode_H_1);
+                coder1(variablee, "I", decode_I_1);
+                coder1(variablee, "J", decode_J_1);
+                coder1(variablee, "K", decode_K_1);
+                coder1(variablee, "L", decode_L_1);
+                coder1(variablee, "M", decode_M_1);
+                coder1(variablee, "N", decode_N_1);
+                coder1(variablee, "O", decode_O_1);
+                coder1(variablee, "P", decode_P_1);
+                coder1(variablee, "Q", decode_Q_1);
+                coder1(variablee, "R", decode_R_1);
+                coder1(variablee, "S", decode_S_1);
+                coder1(variablee, "T", decode_T_1);
+                coder1(variablee, "U", decode_U_1);
+                coder1(variablee, "V", decode_V_1);
+                coder1(variablee, "W", decode_W_1);
+                coder1(variablee, "X", decode_X_1);
+                coder1(variablee, "Y", decode_Y_1);
+                coder1(variablee, "Z", decode_Z_1);
+                coder1(variablee, "ä", decode_ä_1);
+                coder1(variablee, "ö", decode_ö_1);
+                coder1(variablee, "ü", decode_ü_1);
+                coder1(variablee, "Ä", decode_Ä_1);
+                coder1(variablee, "Ö", decode_Ö_1);
+                coder1(variablee, "Ü", decode_Ü_1);
+                coder1(variablee, "1", decode_1_1);
+                coder1(variablee, "2", decode_2_1);
+                coder1(variablee, "3", decode_3_1);
+                coder1(variablee, "4", decode_4_1);
+                coder1(variablee, "5", decode_5_1);
+                coder1(variablee, "6", decode_6_1);
+                coder1(variablee, "7", decode_7_1);
+                coder1(variablee, "8", decode_8_1);
+                coder1(variablee, "9", decode_9_1);
+                coder1(variablee, "0", decode_0_1);
+                coder1(variablee, " ", decode_space_1);
+                coder1(variablee, "^", decode_caret_1);
+                coder1(variablee, "°", decode_bullet_1);
+                coder1(variablee, "!", decode_exclamation_mark_1);
+                coder1(variablee, '"', decode_quotation_mark_1);
+                coder1(variablee, "§", decode_section_sign_1);
+                coder1(variablee, "$", decode_dollar_sign_1);
+                coder1(variablee, "%", decode_percent_1);
+                coder1(variablee, "&", decode_and_1);
+                coder1(variablee, "/", decode_slash_1);
+                coder1(variablee, "\\", decode_backslash_1);
+                coder1(variablee, "(", decode_bracket_left_1);
+                coder1(variablee, ")", decode_bracket_right_1);
+                coder1(variablee, "{", decode_curly_bracket_left_1);
+                coder1(variablee, "}", decode_curly_bracket_right_1);
+                coder1(variablee, "[", decode_square_bracket_left_1);
+                coder1(variablee, "]", decode_square_bracket_right_1);
+                coder1(variablee, "<", decode_angle_bracket_left_1);
+                coder1(variablee, ">", decode_angle_bracket_right_1);
+                coder1(variablee, "=", decode_equal_1);
+                coder1(variablee, "?", decode_question_mark_1);
+                coder1(variablee, "´", decode_acute_accent_1);
+                coder1(variablee, "`", decode_grave_accent_1);
+                coder1(variablee, "+", decode_plus_1);
+                coder1(variablee, "*", decode_asterisk_1);
+                coder1(variablee, "~", decode_tilde_1);
+                coder1(variablee, "#", decode_hash_1);
+                coder1(variablee, "-", decode_minus_1);
+                coder1(variablee, "_", decode_understrike_1);
+                coder1(variablee, ".", decode_dot_1);
+                coder1(variablee, ":", decode_colon_1);
+                coder1(variablee, ",", decode_comma_1);
+                coder1(variablee, ";", decode_semicolon_1);
+                coder1(variablee, "@", decode_at_1);
+                coder1(variablee, "|", decode_pipe_1);
+                coder1(variablee, "'", decode_apostroph_1);
+                coder1(variablee, "€", decode_euro_1);
+
+                dirgesamt += "0sL6V1}bT2!M#wO";
+                repeat = 1;
+                gesamt = dirgesamt;
+                gesamt2 = dirgesamt2;
+            }
+        }
+        if(repeat == 0){
+            all(gesamt, "");
+            repeat = 1;
+            exit = true;
+        }
+        if(repeat == 1 && exit == false){
+            all(gesamt2, gesamt);
+            repeat = 2;
+            exit = true;
+        }
+        if(repeat == 2 && exit == false){
+            all(gesamt2, gesamt);
+            repeat = 1;
+            exit = true;
+        }
+        
+    });
+    if(gesamt2 > gesamt){
+        navigator.clipboard.writeText(gesamt2);
+    }else{
+        navigator.clipboard.writeText(gesamt);
+    }
+}
+
+function degenerate(){
+    let output = document.querySelector('#output').value;
+    const detext = output.split("0sL6V1}bT2!M#wO");
+    // alert(detext);
+    var degesamt;
+    var degesamt2;
+    var derepeat = 0;
+    var deexit = new Boolean();
+    detext.forEach(devariablee => {
+        deexit = false;
+        deweg = false;
+        function all(dedirgesamt, dedirgesamt2){
+            if(true == true){
+                function coder1(devari, deresult, deletter){
+                    if(devari == deletter){
+                        dedirgesamt = dedirgesamt2 + deresult;
+                    }
+                }
+                coder1(devariablee, "a", decode_a_1);
+                coder1(devariablee, "b", decode_b_1);
+                coder1(devariablee, "c", decode_c_1);
+                coder1(devariablee, "a", decode_a_1);
+                coder1(devariablee, "b", decode_b_1);
+                coder1(devariablee, "c", decode_c_1);
+                coder1(devariablee, "d", decode_d_1);
+                coder1(devariablee, "e", decode_e_1);
+                coder1(devariablee, "f", decode_f_1);
+                coder1(devariablee, "g", decode_g_1);
+                coder1(devariablee, "h", decode_h_1);
+                coder1(devariablee, "i", decode_i_1);
+                coder1(devariablee, "j", decode_j_1);
+                coder1(devariablee, "k", decode_k_1);
+                coder1(devariablee, "l", decode_l_1);
+                coder1(devariablee, "m", decode_m_1);
+                coder1(devariablee, "n", decode_n_1);
+                coder1(devariablee, "o", decode_o_1);
+                coder1(devariablee, "p", decode_p_1);
+                coder1(devariablee, "q", decode_q_1);
+                coder1(devariablee, "r", decode_r_1);
+                coder1(devariablee, "s", decode_s_1);
+                coder1(devariablee, "t", decode_t_1);
+                coder1(devariablee, "u", decode_u_1);
+                coder1(devariablee, "v", decode_v_1);
+                coder1(devariablee, "w", decode_w_1);
+                coder1(devariablee, "x", decode_x_1);
+                coder1(devariablee, "y", decode_y_1);
+                coder1(devariablee, "z", decode_z_1);
+                coder1(devariablee, "ß", decode_ß_1);
+                coder1(devariablee, "A", decode_A_1);
+                coder1(devariablee, "B", decode_B_1);
+                coder1(devariablee, "C", decode_C_1);
+                coder1(devariablee, "D", decode_D_1);
+                coder1(devariablee, "E", decode_E_1);
+                coder1(devariablee, "F", decode_F_1);
+                coder1(devariablee, "G", decode_G_1);
+                coder1(devariablee, "H", decode_H_1);
+                coder1(devariablee, "I", decode_I_1);
+                coder1(devariablee, "J", decode_J_1);
+                coder1(devariablee, "K", decode_K_1);
+                coder1(devariablee, "L", decode_L_1);
+                coder1(devariablee, "M", decode_M_1);
+                coder1(devariablee, "N", decode_N_1);
+                coder1(devariablee, "O", decode_O_1);
+                coder1(devariablee, "P", decode_P_1);
+                coder1(devariablee, "Q", decode_Q_1);
+                coder1(devariablee, "R", decode_R_1);
+                coder1(devariablee, "S", decode_S_1);
+                coder1(devariablee, "T", decode_T_1);
+                coder1(devariablee, "U", decode_U_1);
+                coder1(devariablee, "V", decode_V_1);
+                coder1(devariablee, "W", decode_W_1);
+                coder1(devariablee, "X", decode_X_1);
+                coder1(devariablee, "Y", decode_Y_1);
+                coder1(devariablee, "Z", decode_Z_1);
+                coder1(devariablee, "ä", decode_ä_1);
+                coder1(devariablee, "ö", decode_ö_1);
+                coder1(devariablee, "ü", decode_ü_1);
+                coder1(devariablee, "Ä", decode_Ä_1);
+                coder1(devariablee, "Ö", decode_Ö_1);
+                coder1(devariablee, "Ü", decode_Ü_1);
+                coder1(devariablee, "1", decode_1_1);
+                coder1(devariablee, "2", decode_2_1);
+                coder1(devariablee, "3", decode_3_1);
+                coder1(devariablee, "4", decode_4_1);
+                coder1(devariablee, "5", decode_5_1);
+                coder1(devariablee, "6", decode_6_1);
+                coder1(devariablee, "7", decode_7_1);
+                coder1(devariablee, "8", decode_8_1);
+                coder1(devariablee, "9", decode_9_1);
+                coder1(devariablee, "0", decode_0_1);
+                coder1(devariablee, " ", decode_space_1);
+                coder1(devariablee, "^", decode_caret_1);
+                coder1(devariablee, "°", decode_bullet_1);
+                coder1(devariablee, "!", decode_exclamation_mark_1);
+                coder1(devariablee, '"', decode_quotation_mark_1);
+                coder1(devariablee, "§", decode_section_sign_1);
+                coder1(devariablee, "$", decode_dollar_sign_1);
+                coder1(devariablee, "%", decode_percent_1);
+                coder1(devariablee, "&", decode_and_1);
+                coder1(devariablee, "/", decode_slash_1);
+                coder1(devariablee, "\\", decode_backslash_1);
+                coder1(devariablee, "(", decode_bracket_left_1);
+                coder1(devariablee, ")", decode_bracket_right_1);
+                coder1(devariablee, "{", decode_curly_bracket_left_1);
+                coder1(devariablee, "}", decode_curly_bracket_right_1);
+                coder1(devariablee, "[", decode_square_bracket_left_1);
+                coder1(devariablee, "]", decode_square_bracket_right_1);
+                coder1(devariablee, "<", decode_angle_bracket_left_1);
+                coder1(devariablee, ">", decode_angle_bracket_right_1);
+                coder1(devariablee, "=", decode_equal_1);
+                coder1(devariablee, "?", decode_question_mark_1);
+                coder1(devariablee, "´", decode_acute_accent_1);
+                coder1(devariablee, "`", decode_grave_accent_1);
+                coder1(devariablee, "+", decode_plus_1);
+                coder1(devariablee, "*", decode_asterisk_1);
+                coder1(devariablee, "~", decode_tilde_1);
+                coder1(devariablee, "#", decode_hash_1);
+                coder1(devariablee, "-", decode_minus_1);
+                coder1(devariablee, "_", decode_understrike_1);
+                coder1(devariablee, ".", decode_dot_1);
+                coder1(devariablee, ":", decode_colon_1);
+                coder1(devariablee, ",", decode_comma_1);
+                coder1(devariablee, ";", decode_semicolon_1);
+                coder1(devariablee, "@", decode_at_1);
+                coder1(devariablee, "|", decode_pipe_1);
+                coder1(devariablee, "'", decode_apostroph_1);
+                coder1(devariablee, "€", decode_euro_1);
+
+                derepeat = 1;
+                degesamt = dedirgesamt;
+                degesamt2 = dedirgesamt2;
+            }
+        }
+        if(derepeat == 0){
+            all(degesamt, "");
+            derepeat = 1;
+            deexit = true;
+        }
+        if(derepeat == 1 && deexit == false){
+            all(degesamt2, degesamt);
+            derepeat = 2;
+            deexit = true;
+        }
+        if(derepeat == 2 && deexit == false){
+            all(degesamt2, degesamt);
+            derepeat = 1;
+            deexit = true;
+        }
+        
+    });
+    if(degesamt2 > degesamt){
+        navigator.clipboard.writeText(degesamt2);
+    }else{
+        navigator.clipboard.writeText(degesamt);
+    }
+}
